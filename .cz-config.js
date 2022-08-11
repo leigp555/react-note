6ea7e575a4534b172df52d09909ea5c199c37f17
leigp <907090585@qq.com>
2022-08-11 11:09:41 +0800

    docs: 初始化

--- .cz-config.js
+++ .cz-config.js
@@ -2,28 +2,27 @@ module.exports = {
   // type 类型（定义之后，可通过上下键选择）
   types: [
     { value: 'feat', name: 'feat:     新增功能' },
-    { value: 'fix', name: 'fix:      修复 bug' },
-    { value: 'docs', name: 'docs:     文档变更' },
-    { value: 'style', name: 'style:    代码格式（不影响功能，例如空格、分号等格式修正）' },
     { value: 'refactor', name: 'refactor: 代码重构（不包括 bug 修复、功能新增）' },
+    { value: 'chore', name: 'chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
+    { value: 'revert', name: 'revert:   回滚 commit' },
+    { value: 'docs', name: 'docs:     文档变更' },
+    { value: 'fix', name: 'fix:      修复 bug' },
+    { value: 'style', name: 'style:    代码格式（不影响功能，例如空格、分号等格式修正）' },
     { value: 'perf', name: 'perf:     性能优化' },
     { value: 'test', name: 'test:     添加、修改测试用例' },
     { value: 'build', name: 'build:    构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）' },
     { value: 'ci', name: 'ci:       修改 CI 配置、脚本' },
-    { value: 'chore', name: 'chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
-    { value: 'revert', name: 'revert:   回滚 commit' }
   ],
 
   // scope 类型（定义之后，可通过上下键选择）
   scopes: [
     ['components', '组件相关'],
+    ['other', '其他修改'],
+    ['deps', '项目依赖'],
     ['hooks', 'hook 相关'],
     ['utils', 'utils 相关'],
-    ['element-ui', '对 element-ui 的调整'],
     ['styles', '样式相关'],
-    ['deps', '项目依赖'],
     ['auth', '对 auth 修改'],
-    ['other', '其他修改'],
     // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
     ['custom', '以上都不是？我要自定义']
   ].map(([value, description]) => {
@@ -72,7 +71,7 @@ module.exports = {
   allowBreakingChanges: ['feat', 'fix'],
 
   // 跳过要询问的步骤
-  // skipQuestions: ['body', 'footer'],
+  skipQuestions: ['body', 'footer'],
 
   // subject 限制长度
   subjectLimit: 100,
